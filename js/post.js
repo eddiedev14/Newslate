16c8903461143da408d7053da02a6b1997f00d8c
Eddie Santiago Delgado <eddiesantiago122007@gmail.com>
2024-02-11 18:28:40 -0500

    Start of the Transcription
    Once the Word Document has been written, it's time to transcript it to the blog post, at the moment I have passed 5 pages to the website

---
+++ js/post.js
@@ -0,0 +1,8 @@
+var contentSticky = new VanillaSticky({
+    HTMLElement: document.querySelector('.sidebar'),
+    indents: {
+        top: 70,
+        bottom: 10
+    }
+});
+contentSticky.init();
