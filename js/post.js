7bff883dc74c830e79296dd9ae047dfd98bce3ca
Eddie Santiago Delgado <eddiesantiago122007@gmail.com>
2024-02-14 18:20:01 -0500

    Page Finished
    All the content of the word document was correctly transcribed adding new functionalities while maintaining the responsive design.

--- js/post.js
+++ js/post.js
@@ -1,4 +1,4 @@
-var contentSticky = new VanillaSticky({
+let contentSticky = new VanillaSticky({
     HTMLElement: document.querySelector('.sidebar'),
     indents: {
         top: 70,
@@ -6,3 +6,22 @@ var contentSticky = new VanillaSticky({
     }
 });
 contentSticky.init();
+
+//Swiper
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 'auto',
+    spaceBetween: 40,
+    slidesPerGroup: 1,   
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+      },
+    
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+  });
